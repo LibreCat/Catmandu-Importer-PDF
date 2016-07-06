15e402a35eaaf2ca394b88ee0f380068e9231760
Nicolas Franck <nicolas.franck@ugent.be>
2016-07-06 16:07:42 +0200

    travis integration

--- lib/Catmandu/Importer/PDF.pm
+++ lib/Catmandu/Importer/PDF.pm
@@ -68,9 +68,22 @@ sub DESTROY {
     my ($self) = @_;
     close($self->fh);
 }
+
+=encoding utf8
+
 =head1 NAME
 
 Catmandu::Importer::PDF - Catmandu importer to extract data from one pdf
+
+=begin markdown
+
+# STATUS
+
+[![Build Status](https://travis-ci.org/LibreCat/Catmandu-Importer-PDF.svg?branch=master)](https://travis-ci.org/LibreCat/Catmandu-Importer-PDF)
+[![Coverage](https://coveralls.io/repos/LibreCat/Catmandu-Importer-PDF/badge.png?branch=master)](https://coveralls.io/r/LibreCat/Catmandu-Importer-PDF)
+[![CPANTS kwalitee](http://cpants.cpanauthors.org/dist/Catmandu-Importer-PDF.png)](http://cpants.cpanauthors.org/dist/Catmandu-Importer-PDF)
+
+=end markdown
 
 =head1 SYNOPSIS
 
@@ -97,22 +110,26 @@ $importer->each(sub{
 
 =head1 EXAMPLE OUTPUT IN YAML
 
-document:
-  author: ~
-  creation_date: 1207274644
-  creator: PDFplus
-  keywords: ~
-  metadata: ~
-  modification_date: 1421574847
-  producer: "Nobody at all"
-  subject: ~
-  title: "Hello there"
-  version: PDF-1.6
-pages:
-- label: Cover Page
-  height: 878
-  width: 595
-  text: "Hello world"
+=begin text
+
+    document:
+      author: ~
+      creation_date: 1207274644
+      creator: PDFplus
+      keywords: ~
+      metadata: ~
+      modification_date: 1421574847
+      producer: "Nobody at all"
+      subject: ~
+      title: "Hello there"
+      version: PDF-1.6
+    pages:
+    - label: Cover Page
+      height: 878
+      width: 595
+      text: "Hello world"
+
+=end text
 
 =head1 INSTALL
 
@@ -141,11 +158,20 @@ In order to install this package you need the following system packages installe
 Centos 6 only has poppler-glib 0.12. So you need at least Centos 7.
 Or you can compile the package.
 
+=item Ubuntu
+
+* libpoppler-glib8
+
+* libpoppler-glib-dev
+
+* gobject-introspection
+
 =back
 
 =head1 NOTES
 
 * returns only one record, compared to other Catmandu importers
+
 * all pages are stored in one record. For large documents this can be memory intensive.
 
 =head1 AUTHORS
